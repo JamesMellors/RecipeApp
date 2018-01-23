36d4ab4fa682a649a450d5f88212eda885b4e913
James Mellors <jamesmellors@survey-me.com>
2018-01-23 16:55:35 +0000

    Start on creating database for storage

---
+++ RecepeApp/RecepeApp/ContactDatabase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace RecepeApp
+{
+    class ContactDatabase
+    {
+        private SQLiteConnection dataBase;
+
+
+
+        public ContactDatabase()
+        {
+            
+        }
+    }
+}
